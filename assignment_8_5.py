0f42bf6016ef1fa3d6398a4e9389c09013e7477a
8chill3s <39828278+8chill3s@users.noreply.github.com>
2018-06-01 15:49:12 -0400

    Update assignment_8_5.py

--- assignment_8_5.py
+++ assignment_8_5.py
@@ -1,11 +1,20 @@
 #open mbox-short.txt
 
 fname = input("Enter file name: ")
-open(fname)
-
-if len(fname) < 1 : fname = "mbox-short.txt"
-
 fh = open(fname)
+#lst = list()
 count = 0
 
-print("There were", count, "lines in
+for line in fh:             #loop through text file
+    
+    for words in line:      #loop through line
+        line =  line.rstrip()   #strip white space right
+        words = line.split()    #split line into words
+        
+    if "From" in words:
+        print(words[1])  #print address			
+        count = count + 1       #counter 
+        
+    
+    
+print('There were',count,'lines in the file with From as the first word')                         
