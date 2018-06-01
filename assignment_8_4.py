6d096466e144a2b9fbc01badb5faf98d5c94a6be
8chill3s <39828278+8chill3s@users.noreply.github.com>
2018-06-01 12:47:25 -0400

    Update assignment_8_4.py

--- assignment_8_4.py
+++ assignment_8_4.py
@@ -4,12 +4,10 @@ fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()
 
-for line in fh:             #loop through text 
-    line = line.strip()   #strip right side
+for line in fh:             #loop through text file
     words = line.split()  #split line into words
-
   
-    for word in words:     #loop through line
+    for word in words:     #loop through line 
         if word not in lst:  #append word to list
         	lst.append(word)
     
