74a7829cf320f4a13b5b57b72c8ffa5b7c0ced72
8chill3s <39828278+8chill3s@users.noreply.github.com>
2018-06-05 09:04:42 -0400

    Update ex_9_4.py

--- ex_9_4.py
+++ ex_9_4.py
@@ -1,6 +1,6 @@
 
 
-#  open  mbox-short.txt and figure out who has the sent the greatest number of mail messages
+#  open  mbox-short.txt 
 name = input("Enter file:")
 try:
     fh = open(name)
@@ -12,37 +12,33 @@ except:
 #create a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file.
 count_names = dict()
 
-#looks for 'From ' lines and takes the second word of those lines as the person who sent the mail
+#looks for 'From' lines and takes the second word of those lines as the person who sent the mail
 
 for line in fh:  # loop through text file
     line = line.rstrip()  # strip white space right
     if not line.startswith("From:"): continue       #look for 'From'
-    names = line.split()  # split line into words
+    words = line.split()  # split line into words
 
 
-    for name in names:
-        count_names[name] = count_names.get(name,0) +1     #make histogram
+    for name in words:
+        if "@" in name:
+         count_names[name] = count_names.get(name,0) +1     #idiom for make histogram
 
-#print(count_names)
-
-#for name in count_names:
- #   print(name,count_names[name])
-
+	#print(words)
+    
 #for key,value in count_names.items():             #analyze key value pair in dictionary
  #   print(key, value)
 
+    
+    
+    
 #After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
 bigcount = None
 bigword = None
 
 for name,count in count_names.items():              #maximum loop for largest number
-    if bigcount is None or count > bigcount:
-        bigword = names
+    if bigcount is None or count > bigcount:		# determine max key value 
+        bigword = words								# words from lines
         bigcount = count
 
-print(bigword, bigcount)
-
-#if len(name) < 1 : name = "mbox-short.txt"
-
-
-
+print(bigword[1], bigcount)
